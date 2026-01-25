181e98c58129da05a14a4ba031a77cb7784ea4cf
Md Fuyad Hassan <fuyadswe@gmail.com>
2026-01-25 16:29:59 +0600

    Fix: Remove WhiteNoise storage dependency to fix build

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
